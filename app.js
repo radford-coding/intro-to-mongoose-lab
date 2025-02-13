e717cf13a66c5e11b97a86f758ce27537512cace
Matt Radford <quinquex@gmail.com>
2025-02-13 13:20:27 -0800

    process flow logic

--- app.js
+++ app.js
@@ -3,8 +3,30 @@ const dotenv = require('dotenv');
 dotenv.config();
 const mongoose = require('mongoose');
 
-)
 
-const username = prompt('What is your name? ');
 
-console.log(`Your name is ${username}`);
+
+
+const connect = async () => {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log('You have entered the CRM');
+
+    await runQueries();
+
+};
+
+const runQueries = async () => {
+    // do logic of stuff in app
+    const username = prompt('What is your name? ');
+    if (username === 'quit') {
+        await mongoose.disconnect();
+        console.log('exiting. goodbyeeee.......');
+        process.exit(); // not sure why/when this is needed - seems like doesn't matter with Node.js
+    } else {
+        console.log(`Your name is ${username}`);
+        console.log('doing app stuffs');
+        await runQueries();
+    }
+};
+
+connect();
